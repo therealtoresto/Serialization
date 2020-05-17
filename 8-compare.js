0dfe21f88fcb6e385c95c5351b927f98c79bc6fe
TORESTO <therealtoresto@gmail.com>
2020-05-17 12:19:58 +0300

    compare serializors

---
+++ 8-compare.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const v8 = require('v8');
+const fs = require('fs');
+//const BSON = require('bson');
+//const bson = new BSON();
+
+const persons = [
+    { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
+    { name: 'Victor Glushkov', city: 'Rostov on Don', born: 1923 },
+    { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
+    { name: 'Mao Zedoung', city: 'Shaoshan', born: 1893},
+    { name: 'Rene Descartes', city: 'La Haye en Touraine', born: 1596 },
+];
+
+
+const v8Data = v8.serialize(persons);
+const v8File = './data.v8';
+fs.writeFile(v8File, v8Data, () => {
+    console.log('Saved ' + v8File);
+});
+
+const jsonData = JSON.stringify(persons);
+const jsonFile = './data.json';
+fs.writeFile(jsonFile, jsonData, () => {
+    console.log('Saved ' + jsonFile);
+});
+
+// const bsonData = bson.serialize(persons);
+// const bsonFile = './data.bson';
+// fs.writeFile(bsonFile, bsonData, () => {
+//     console.log('Saved ' + bsonFile);
+// });
